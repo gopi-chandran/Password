c618a44d91aaad3999000ef371d8e375bb9f72d7
gopichandran13 <iamgopi1321@gmail.com>
2024-10-01 19:04:53 +0530

    Password
    Added Password generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PasswordGenerator } from './PasswordGenerator'
+
+const App = () => {
+  return (
+    <>
+    <PasswordGenerator />
+    </>
+  )
+}
+
+export default App
